37102912888d6f07ab74a713fa6a5856affb81ce
Josué BAYIDIKILA <josue-dev@outlook.fr>
2024-01-25 09:43:32 +0100

    correction postgre

--- src/config/sequelizeInstance.ts
+++ src/config/sequelizeInstance.ts
@@ -2,8 +2,8 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('cinemaseance', 'root', 'TUPvqBxLCMHrB21QryECugKnEVyPna3G', {
   host: 'dpg-cmp1l30l5elc73fltl40-a',
-//   dialect: 'postgres'
-  dialect: 'mysql'
+  dialect: 'postgres'
+  // dialect: 'mysql'
 });
 
 export default sequelize;
