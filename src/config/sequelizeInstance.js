bdc0c2742ff3bc2388a2c744bcac5bb8e79945fa
Josué BAYIDIKILA <josue-dev@outlook.fr>
2024-01-25 09:44:21 +0100

    update typescript files

--- src/config/sequelizeInstance.js
+++ src/config/sequelizeInstance.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize('cinemaseance', 'root', 'TUPvqBxLCMHrB21QryECugKnEVyPna3G', {
     host: 'dpg-cmp1l30l5elc73fltl40-a',
-    //   dialect: 'postgres'
-    dialect: 'mysql'
+    dialect: 'postgres'
+    // dialect: 'mysql'
 });
 exports.default = sequelize;
